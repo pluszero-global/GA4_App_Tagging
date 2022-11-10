37dd80dc51a67567aa817b7df9d61ff391070d5c
Pluszero <77051224+pluszero-global@users.noreply.github.com>
2022-11-10 17:01:06 +0900

    Update controller function.swift

--- controller function.swift
+++ controller function.swift
@@ -1,22 +1,23 @@
 func userContentController(_ userContentController: WKUserContentController,
-                             didReceive message: WKScriptMessage) {
-      guard let body = message.body as? [String: Any] else { return }
-      guard let command = body["command"] as? String else { return }
-      guard let name = body["name"] as? String else { return }
+                                 didReceive message: WKScriptMessage) {
+          guard let body = message.body as? [String: Any] else { return }
+          guard let command = body["command"] as? String else { return }
+          
         
-      if command == "setUserProperty" {
-        guard let params = body["parameters"] as? [String: NSObject] else { return }
-        
-        for (key, value) in params {
-            if(key == "user_id"){
-                Analytics.setUserID(value as? String)
-            } else{
-                Analytics.setUserProperty(value as? String, forName: key)
+          if command == "setUserProperty" {
+            guard let params = body["parameters"] as? [String: NSObject] else { return }
+            
+            for (key, value) in params {
+                if(key == "user_id"){
+                    Analytics.setUserID(value as? String)
+                    
+                } else{
+                    Analytics.setUserProperty(value as? String, forName: key)
+                }
             }
+          } else if command == "logEvent" {
+              guard let name = body["name"] as? String else { return }
+              guard let params = body["parameters"] as? [String: NSObject] else { return }
+            Analytics.logEvent(name, parameters: params)
+          }
         }
-        
-      } else if command == "logEvent" {
-        guard let params = body["parameters"] as? [String: NSObject] else { return }
-        Analytics.logEvent(name, parameters: params)
-      }
-    }
